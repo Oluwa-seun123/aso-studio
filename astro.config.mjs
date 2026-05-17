7008d985ffd1bd46a3a393f584bcbcb5617fa07f
Oluwa-seun123 <165198112+Oluwa-seun123@users.noreply.github.com>
2026-05-17 23:04:55 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  output: "static",
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }

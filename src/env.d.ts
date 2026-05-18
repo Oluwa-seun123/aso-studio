c149100bd1459eb61553dc677b0453940ef591c3
Oluwa-seun123 <165198112+Oluwa-seun123@users.noreply.github.com>
2026-05-18 11:44:12 +0100

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,1 @@
-/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-  readonly SITE_URL: string;
-  readonly SITE_TITLE: string;
-  readonly SITE_DESCRIPTION: string;
-  readonly CONTACT_EMAIL: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
